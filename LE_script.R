70cb029b9f16cd484402f6e6177f573ff36eb1cb
Becky Trippier <b.tripps@outlook.com>
2023-04-20 17:49:07 +0100

    Added metadata to blank script

---
+++ LE_script.R
@@ -0,0 +1,19 @@
+## ############################## ##
+
+##
+## Script Name: Script 1
+##
+## Description:  Test Script
+## 
+## Author: BTripps, Natural England
+## 
+## Licence: MIT Licence  
+## Date Created: 2023-04-20
+## 
+## Date Last Modified:2023-04-20
+## 
+## Versioning:
+## R version 4.2.3 (2023-03-15 ucrt)
+## Dependencies:
+## 
+## ############################## ##

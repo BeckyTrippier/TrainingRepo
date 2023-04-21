5f1e9fe9130b498330ec33477e04e1e4a7b8c52a
Becky Trippier <b.tripps@outlook.com>
2023-04-21 10:37:59 +0100

    removed LE_script info

--- LE_script.R
+++ LE_script.R
@@ -1,16 +1,16 @@
-## ############################## ##
-
+## ############################## 
 ##
-## Script Name: Script 1
 ##
-## Description:  Test Script
+## Script Name:
+##
+## Description:  
 ## 
-## Author: BTripps, Natural England
+## Author: NE_Ev, Natural England
 ## 
 ## Licence: MIT Licence  
-## Date Created: 2023-04-20
+## Date Created: 2023-04-21
 ## 
-## Date Last Modified:2023-04-20
+## Date Last Modified:
 ## 
 ## Versioning:
 ## R version 4.2.3 (2023-03-15 ucrt)
